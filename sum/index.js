9c70dc30823d6e6473f47204283af68f82920453
Jenniferjepp <nelliferjj@gmail.com>
2024-12-29 21:47:39 +0100

    SUM - markerade divars summa
    Kodat vidare i klick eventet för divarna. Jag skapaden en array som innehåller alla klickade divars nummer. Sen skapade jag en variabel "sumOfMarked" som innehåller summan av alla markerade nummer från arrayen. div1 får textcontent sumOfMarked.

--- sum/index.js
+++ sum/index.js
@@ -28,12 +28,28 @@ const divsBoxDOM = document.querySelector("#divs-box");
 const resetButtonDOM = document.querySelector("#reset-button");
 
 
+
+let allMarkedArray = [];
 // när någon div i containern "divsBox" klickas ==> de får grön backgrundsfärg.
 divsBoxDOM.addEventListener("click", function (eventObj) {
     if (eventObj.target.classList.contains("divis")) {
         eventObj.target.style.backgroundColor = "darkseagreen";
     }
+    console.log(eventObj.target);
+    
+    // pushar in alla markerade nummer i en array.
+    allMarkedArray.push(Number(eventObj.target.textContent));
+    console.log(allMarkedArray);
+
+    // loopar igenom arrayen med alla markerade nummer och adderar dessa, summan sparas i variabeln "sumOfMarked" och det blir div2:s textcontent.
+    let sumOfMarked = 0;
+    for (let i = 0; i < allMarkedArray.length; i++) {
+        sumOfMarked += allMarkedArray[i];
+    }
+    console.log(sumOfMarked);
+    div2.textContent = sumOfMarked;
 });
+
 
 // när "reset"-knappen klickas
 resetButtonDOM.addEventListener("click", function () {
@@ -43,14 +59,15 @@ resetButtonDOM.addEventListener("click", function () {
         allDivs[i].style.backgroundColor = "";
     }
     // text i div2 går tillbaks till "-"
+    div2.textContent = "-";
 
 });
 
-// click event på divar
 
 // när "create"-kanppen klickas ==> funtionen som skriver ut summan anropas.
 createButtonDOM.addEventListener("click", function() {
     sumOfAll();
+    div2.textContent = "-";
 });
 
 // funktionen som räknar ihop och skriver ut den anropas inte förens divarna är skapade.
