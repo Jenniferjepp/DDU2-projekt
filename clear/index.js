6ccdac8eaedf14cd81af2f8999cd78580fc2f7fa
Jenniferjepp <nelliferjj@gmail.com>
2025-01-03 19:32:41 +0100

    CLEAR spegelvändning av färger
    Har kodat så att divarna går spegelvänt tillbaks till de olika bakgrundsfärgerna,

--- clear/index.js
+++ clear/index.js
@@ -7,41 +7,59 @@
 // klick på RÖD div = färgerna försvinner + ny siffra i diven/ divarna
 
 
-
 const divsBoxDOM = document.querySelector("#divs-box");
 const clearButtonDOM = document.querySelector("#clear-button");
 
-// div blir BLÅ vid mouseover
+
+// div blir BLÅ vid mouseover  /  röd vid spegelvändning
 // använder eventobjektet som innehåller info om vilket element i griden som musen startade eventet.
 divsBoxDOM.addEventListener("mouseover", (eventObj) => {
     // om eventobjektet innehåller klassen "divis" så ska färgen ändras.
     if (eventObj.target.classList.contains("divis")) {
-        eventObj.target.style.backgroundColor = "skyblue";
+        if (eventObj.target.style.backgroundColor === "tomato") {
+            eventObj.target.style.backgroundColor = "sandybrown";
+        } else {
+            eventObj.target.style.backgroundColor = "skyblue";
+        }
         
     }
 });
 
-// div blir ORANGE vid klick
+// div blir ORANGE vid klick  /  blå vid spegelvändning
 divsBoxDOM.addEventListener("click", function(eventObj) {
     if (eventObj.target.classList.contains("divis")) {
-        eventObj.target.style.backgroundColor = "sandybrown";
-        eventObj.target.textContent = "";
+        if (eventObj.target.style.backgroundColor === "sandybrown") {
+            eventObj.target.style.backgroundColor = "skyblue";
+        } else {
+            eventObj.target.style.backgroundColor = "sandybrown";
+            eventObj.target.textContent = "";
+        }
     }
 });
 
-// div blir RÖD vid mouseout
+// div blir RÖD vid mouseout  /  ursprungsfärg vid spegelvändning
 divsBoxDOM.addEventListener("mouseout", function(eventObj){
     if (eventObj.target.classList.contains("divis")) {
-        eventObj.target.style.backgroundColor = "tomato";
-        eventObj.target.textContent = "";
+        if (eventObj.target.style.backgroundColor === "skyblue") {
+            eventObj.target.style.backgroundColor = "";
+        } else {
+            eventObj.target.style.backgroundColor = "tomato";
+            eventObj.target.textContent = "";
+        }
     }
 });
+
+
 
 // här ska eventet för knappen "clear-button" att ske
 clearButtonDOM.addEventListener("click", function() {
-
+    const allDivs = document.querySelectorAll(".divis");
+    for (let i = 0; i < allDivs.length; i++) {
+        allDivs[i].style.backgroundColor = "";
+    }
 });
 
 
-// behöver fixa spegelvändinngen tillbaks till ornage och sen blå
 // samt att klickad div ändrar random siffra
+
+
